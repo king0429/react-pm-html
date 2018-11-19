e259d5fffbb4dc1220d166ac574f509d5b5efc92
jinhang <jinhang@ninstarscf.com>
2018-11-19 11:22:50 +0800

    editing some words creating projects

--- src/pages/project/project_create/create.js
+++ src/pages/project/project_create/create.js
@@ -13,12 +13,36 @@ class projectCreate extends Component {
     }
   }
   render () {
+    let optionArr = []
+    for (let i = 1; i <= 5; i++) {
+        optionArr.push(<option value={i} key={i}>{i}</option>)
+    }
     return (
       <div className="project_create">
         <h2 className='page_title'>
           <i className="iconfont icon-huowudui"></i>
           <span>创建任务</span>
         </h2>
+        <div className="project_form">
+          <div className="project_line">
+            <div className="project_item">
+              <span>项目名称</span>
+              <input type="text" ref='project_name' maxLength="16" />
+            </div>
+          </div>
+          <div className="project_line">
+            <div className="project_item">
+              <span>项目周期</span>
+              <input type="text" ref='project_duration' maxLength="4" />
+            </div>
+            <div className="project_item">
+              <span>项目级别</span>
+              <select ref='project_class'>
+                {optionArr}
+              </select>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
